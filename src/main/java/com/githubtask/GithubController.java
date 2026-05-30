ec6c85477566b1507d1e46377d3a3c258e48975b
abramovich-maks <maks.abramovich.1995@mail.ru>
2026-05-30 12:13:26 +0200

    Add GitHub service and controller for fetching user repositories

--- src/main/java/com/githubtask/GithubController.java
+++ src/main/java/com/githubtask/GithubController.java
@@ -1,4 +1,39 @@
 package com.githubtask;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/github")
 class GithubController {
+
+    private final GithubService githubService;
+
+    GithubController(GithubService githubService) {
+        this.githubService = githubService;
+    }
+
+    @GetMapping("/{username}/repositories")
+    ResponseEntity<List<RepositoryResponseDto>> getRepositories(@PathVariable String username) {
+        List<Repository> repositories = githubService.getRepositories(username);
+
+        List<RepositoryResponseDto> list = mapToDto(repositories);
+        return ResponseEntity.ok(list);
+    }
+
+    private List<RepositoryResponseDto> mapToDto(final List<Repository> repositories) {
+        return repositories.stream()
+                .map(repo -> {
+                    List<BranchResponseDto> mappedBranches = repo.branches().stream()
+                            .map(branch -> new BranchResponseDto(branch.name(), branch.lastCommitSha()))
+                            .toList();
+                    return new RepositoryResponseDto(repo.name(), repo.ownerLogin(), mappedBranches);
+                })
+                .toList();
+    }
 }
--- src/main/java/com/githubtask/GithubService.java
+++ src/main/java/com/githubtask/GithubService.java
@@ -1,4 +1,38 @@
 package com.githubtask;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 class GithubService {
+
+    private final GithubClient githubClient;
+
+    GithubService(GithubClient githubClient) {
+        this.githubClient = githubClient;
+    }
+
+    List<Repository> getRepositories(String username) {
+        return githubClient.getRepositories(username)
+                .stream()
+                .filter(repository -> !repository.fork())
+                .map(this::mapRepository)
+                .toList();
+    }
+
+    private Repository mapRepository(GithubRepositoryResponse repositoryResponse) {
+        String owner = repositoryResponse.owner().login();
+        String repositoryName = repositoryResponse.name();
+
+        List<Branch> branches = githubClient.getBranches(owner, repositoryName)
+                .stream()
+                .map(branch -> new Branch(
+                        branch.name(),
+                        branch.commit().sha()
+                ))
+                .toList();
+
+        return new Repository(repositoryName, owner, branches);
+    }
 }
